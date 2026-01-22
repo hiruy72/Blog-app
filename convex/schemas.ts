22903a378c4ca0554e741d5baec2e2f029b6c7fd
Hiruy Legesse <hiruyadane@gmail.com>
2026-01-21 20:29:10 -0800

    Add table fields to post schema

--- convex/schemas.ts
+++ convex/schemas.ts
@@ -1,10 +1,12 @@
 import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
 
 export default defineSchema({
 
     posts: defineTable({
-        title: "string",
-        content: "string",
+        title: v.string(),
+        body : v.string(),
+        authorId: v.string(),
     })
 
 })
